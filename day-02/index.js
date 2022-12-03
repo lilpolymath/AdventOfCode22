af10f55bf9884fe39bb5f9af8e461cae571e7120
Favour <ayofavour7@gmail.com>
2022-12-03 04:53:32 +0000

    feat: day two -> part two

--- day-02/index.js
+++ day-02/index.js
@@ -1,8 +1,11 @@
 const { readFileSync } = require('fs')
 
 const key = {
-  X: 1, Y: 2, Z: 3
+  A: 1, B: 2, C: 3, X: 1, Y: 2, Z: 3
 }
+
+const win = ["AY", "BZ", "CX",]
+const lose = ["AZ", "CY", "BX"]
 
 const readFile = (filename) => {
   const contents = readFileSync(filename, 'utf-8');
@@ -12,13 +15,14 @@ const readFile = (filename) => {
   return arr
 }
 
-const win = ["AY", "BZ", "CX",]
-const lose = ["AZ", "CY", "BX"]
+const data = readFile('./data.txt')
+
+// --------- PART ONE ------------
 
 const solution = arr => {
 
   const result = arr.map(element => {
-    const mine = element.split(" ")[1]
+    const [, mine] = element.split(" ")
     const parsed = element.replace(/\s/g, '')
 
     let score = key[mine]
@@ -46,5 +50,40 @@ const solution = arr => {
 
 }
 
-const data = readFile('./data.txt')
 console.log(solution(data))
+
+// --------- PART TWO ------------
+
+const winTwo = { "A": "Y", "B": "Z", "C": "X" }
+const loseTwo = { "A": "Z", "C": "Y", "B": "X" }
+
+const solutionTwo = arr => {
+
+  const result = arr.map(element => {
+    const [opp, mine] = element.split(" ")
+
+    let score = 0
+
+    switch (mine) {
+      case "Z":
+        score = key[winTwo[opp]] + 6
+        break
+
+      case "Y":
+        score = key[opp] + 3
+        break
+
+      case "X":
+        score = key[loseTwo[opp]] + 0
+        break
+    }
+
+
+    return score
+  })
+
+  return result.reduce((acc, curr) => acc + curr, 0)
+
+}
+
+console.log(solutionTwo(data))
